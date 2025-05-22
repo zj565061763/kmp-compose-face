6cf1d32815c12f1b06777e0c06ee741ee71fba06
zjun <565061763@qq.com>
2025-05-22 17:13:07 +0800

    FaceView形状逻辑移动到外部

--- composeApp/src/androidMain/kotlin/com/sd/demo/kmp/compose_face/FacePageView.kt
+++ composeApp/src/androidMain/kotlin/com/sd/demo/kmp/compose_face/FacePageView.kt
@@ -1,10 +1,12 @@
 package com.sd.demo.kmp.compose_face
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,6 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,7 +70,7 @@ fun FacePageView(
     modifier = modifier.fillMaxSize(),
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-    Spacer(Modifier.height(128.dp))
+    Spacer(Modifier.height(64.dp))
     Text(
       text = tips.ifEmpty { " " },
       color = MaterialTheme.colorScheme.onSurface,
@@ -75,7 +79,10 @@ fun FacePageView(
     )
     Spacer(Modifier.height(16.dp))
     FaceView(
-      modifier = Modifier.size(160.dp),
+      modifier = Modifier
+        .size(160.dp)
+        .clip(CircleShape)
+        .background(Color.Red),
       vm = vm,
     )
   }
--- lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceView.kt
+++ lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceView.kt
@@ -1,16 +1,12 @@
 package com.sd.lib.kmp.compose_face
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
@@ -26,9 +22,7 @@ fun FaceView(
   }
 
   Box(
-    modifier = modifier
-      .clip(CircleShape)
-      .background(Color.Black),
+    modifier = modifier,
     contentAlignment = Alignment.Center,
   ) {
     if (state.stage !is FaceViewModel.Stage.Finished) {
