b10ba78676251e5c227bd0c9d494fcb66a1b05ac
zjun <565061763@qq.com>
2025-05-23 14:37:32 +0800

    FaceManager.release

--- lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceManager.kt
+++ lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceManager.kt
@@ -3,18 +3,32 @@ package com.sd.lib.kmp.compose_face
 import android.content.Context
 import android.util.Log
 import com.insightface.sdk.inspireface.InspireFace
+import com.sd.lib.kmp.compose_face.FaceManager.init
 
 object FaceManager {
   private var _hasInit = false
 
+  /** 初始化 */
+  @JvmStatic
   fun init(context: Context) {
     if (_hasInit) return
     _hasInit = InspireFace.GlobalLaunch(context.applicationContext, InspireFace.PIKACHU)
     log { "init:$_hasInit" }
   }
 
+  @JvmStatic
   fun compare(a: FloatArray, b: FloatArray): Float {
     return faceCompare(a, b)
+  }
+
+  /** 释放，释放后需要重新调用[init]初始化，才可以继续使用SDK */
+  @JvmStatic
+  fun release() {
+    if (_hasInit) {
+      _hasInit = false
+      val release = InspireFace.GlobalTerminate()
+      log { "release:$release" }
+    }
   }
 
   internal inline fun log(block: () -> String) {
