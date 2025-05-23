5d4bd359bd15e19492f2cefedaf4d1d7044f3b1e
zjun <565061763@qq.com>
2025-05-23 14:39:15 +0800

    CircleShape

--- composeApp/src/androidMain/kotlin/com/sd/demo/kmp/compose_face/FacePageView.kt
+++ composeApp/src/androidMain/kotlin/com/sd/demo/kmp/compose_face/FacePageView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -79,7 +81,7 @@ fun FacePageView(
     FaceView(
       modifier = Modifier
         .size(160.dp)
-//        .clip(CircleShape)
+        .clip(CircleShape)
         .background(Color.Red),
       vm = vm,
     )
