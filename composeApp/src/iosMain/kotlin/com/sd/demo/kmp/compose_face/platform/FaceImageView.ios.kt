89b213ed22175c51ea6a86b7fae5de2d78e7e497
zjun <565061763@qq.com>
2025-05-28 16:55:31 +0800

    FaceImageView.ios

---
+++ composeApp/src/iosMain/kotlin/com/sd/demo/kmp/compose_face/platform/FaceImageView.ios.kt
@@ -0,0 +1,22 @@
+package com.sd.demo.kmp.compose_face.platform
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitView
+import com.sd.lib.kmp.compose_face.FaceImage
+import com.sd.lib.kmp.compose_face.FaceImageWithUIImage
+import platform.UIKit.UIImageView
+
+@Composable
+actual fun FaceImageView(
+  modifier: Modifier,
+  faceImage: FaceImage,
+) {
+  val image = remember(faceImage) { (faceImage as FaceImageWithUIImage).getUIImage() }
+  UIKitView(
+    modifier = modifier,
+    factory = { UIImageView() },
+    update = { it.image = image },
+  )
+}
