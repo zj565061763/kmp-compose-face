d87e52efae06e2c9b6488918efb65b21dd605c4c
zjun <565061763@qq.com>
2025-05-28 17:04:13 +0800

    提示

--- composeApp/src/commonMain/kotlin/com/sd/demo/kmp/compose_face/RouteHome.kt
+++ composeApp/src/commonMain/kotlin/com/sd/demo/kmp/compose_face/RouteHome.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.sd.demo.kmp.compose_face.platform.rememberPermissionUtils
+import kotlinx.coroutines.launch
 
 @Composable
 fun RouteHome(
@@ -17,7 +22,12 @@ fun RouteHome(
   onClickValidate: () -> Unit,
 ) {
   val permissionUtils = rememberPermissionUtils()
-  Scaffold { padding ->
+  val coroutineScope = rememberCoroutineScope()
+  val snackbarHostState = remember { SnackbarHostState() }
+
+  Scaffold(
+    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+  ) { padding ->
     Column(
       modifier = Modifier.fillMaxSize().padding(padding),
       horizontalAlignment = Alignment.CenterHorizontally,
@@ -26,7 +36,15 @@ fun RouteHome(
         Button(onClick = onClickRecord) {
           Text(text = "录入")
         }
-        Button(onClick = onClickValidate) {
+        Button(onClick = {
+          if (ComposeApp.faceData.isEmpty()) {
+            coroutineScope.launch {
+              snackbarHostState.showSnackbar("请先录入人脸")
+            }
+          } else {
+            onClickValidate()
+          }
+        }) {
           Text(text = "验证")
         }
       } else {
