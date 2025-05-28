bd8e1205edfd8b2d76960d1f6c7ca518072f91eb
zjun <565061763@qq.com>
2025-05-28 15:45:59 +0800

    FaceImageWithUIImage

--- lib/src/iosMain/kotlin/com/sd/lib/kmp/compose_face/FaceImageWithUIImage.kt
+++ lib/src/iosMain/kotlin/com/sd/lib/kmp/compose_face/FaceImageWithUIImage.kt
@@ -1,8 +1,74 @@
 package com.sd.lib.kmp.compose_face
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UByteVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.set
+import platform.CoreGraphics.CGBitmapContextCreate
+import platform.CoreGraphics.CGBitmapContextCreateImage
+import platform.CoreGraphics.CGColorSpaceCreateDeviceRGB
+import platform.CoreGraphics.CGColorSpaceRelease
+import platform.CoreGraphics.CGContextRelease
+import platform.CoreGraphics.CGImageAlphaInfo
+import platform.CoreGraphics.CGImageRelease
+import platform.UIKit.UIImage
+import platform.posix.size_t
+
 class FaceImageWithUIImage internal constructor(
   private val imageData: BGRImageData,
 ) : FaceImage {
 
+  fun getUIImage(): UIImage? {
+    return imageData.toUIImage()
+  }
+
   override fun close() = Unit
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun BGRImageData.toUIImage(): UIImage? {
+  return memScoped {
+    val pixelCount = width * height
+    val rgbaSize = pixelCount * 4
+
+    val rgba = allocArray<UByteVar>(rgbaSize)
+
+    var m = 0
+    var n = 0
+    repeat(pixelCount) {
+      val b = data[n++]
+      val g = data[n++]
+      val r = data[n++]
+      rgba[m++] = r
+      rgba[m++] = g
+      rgba[m++] = b
+      rgba[m++] = 255u.toUByte()
+    }
+
+    val bitsPerComponent: size_t = 8uL
+    val bytesPerRow: size_t = (4 * width).toULong()
+
+    val colorSpace = CGColorSpaceCreateDeviceRGB()
+    val bitmapInfo = CGImageAlphaInfo.kCGImageAlphaPremultipliedLast
+
+    val context = CGBitmapContextCreate(
+      data = rgba,
+      width = width.toULong(),
+      height = height.toULong(),
+      bitsPerComponent = bitsPerComponent,
+      bytesPerRow = bytesPerRow,
+      space = colorSpace,
+      bitmapInfo = bitmapInfo.value,
+    )
+
+    val cgImage = CGBitmapContextCreateImage(context) ?: return null
+    val uiImage = UIImage.imageWithCGImage(cgImage)
+
+    CGContextRelease(context)
+    CGImageRelease(cgImage)
+    CGColorSpaceRelease(colorSpace)
+
+    uiImage
+  }
 }
