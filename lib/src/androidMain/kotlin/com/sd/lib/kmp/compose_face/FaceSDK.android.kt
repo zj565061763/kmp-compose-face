442442022a040e9806323ebcf28c082780990edc
zjun <565061763@qq.com>
2025-05-28 14:12:58 +0800

    faceCompare

--- lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceSDK.android.kt
+++ lib/src/androidMain/kotlin/com/sd/lib/kmp/compose_face/FaceSDK.android.kt
@@ -4,6 +4,7 @@ import com.insightface.sdk.inspireface.InspireFace
 import com.insightface.sdk.inspireface.base.FaceFeature
 
 internal actual fun faceCompare(a: FloatArray, b: FloatArray): Float {
+  if (a.isEmpty() || b.isEmpty()) return 0f
   val fa = FaceFeature().apply { this.data = a }
   val fb = FaceFeature().apply { this.data = b }
   return InspireFace.FaceComparison(fa, fb)
--- lib/src/iosMain/kotlin/com/sd/lib/kmp/compose_face/FaceSDK.ios.kt
+++ lib/src/iosMain/kotlin/com/sd/lib/kmp/compose_face/FaceSDK.ios.kt
@@ -1,5 +1,45 @@
 package com.sd.lib.kmp.compose_face
 
+import InspireFace.HFFaceComparison
+import InspireFace.HFFaceFeature
+import InspireFace.HSUCCEED
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.FloatVar
+import kotlinx.cinterop.FloatVarOf
+import kotlinx.cinterop.MemScope
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.readValue
+import kotlinx.cinterop.set
+import kotlinx.cinterop.value
+
+@OptIn(ExperimentalForeignApi::class)
 internal actual fun faceCompare(a: FloatArray, b: FloatArray): Float {
-  return 0f
+  if (a.isEmpty() || b.isEmpty()) return 0f
+  return memScoped {
+    val fa = floatArrayToHFFaceFeature(a)
+    val fb = floatArrayToHFFaceFeature(b)
+
+    val similarityPtr = alloc<FloatVarOf<Float>>()
+    val ret = HFFaceComparison(fa.readValue(), fb.readValue(), similarityPtr.ptr).toInt()
+    if (ret != HSUCCEED) {
+      FaceManager.log { "faceCompare HFFaceComparison failed $ret" }
+      return 0f
+    }
+
+    similarityPtr.value
+  }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun MemScope.floatArrayToHFFaceFeature(array: FloatArray): HFFaceFeature {
+  val dataPtr = allocArray<FloatVar>(array.size)
+  for (i in array.indices) {
+    dataPtr[i] = array[i]
+  }
+  return alloc<HFFaceFeature>().apply {
+    this.data = dataPtr
+  }
 }
